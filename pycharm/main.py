d76a3339b9b13b43b57f4839901089e238ca9708
louisadam21 <louis.adam67@gmail.com>
2020-10-27 09:21:47 +0100

    projet ok

--- pycharm/main.py
+++ pycharm/main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import math
-from functions import fct
+from pycharm.functions import fct
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 1000)
 
-df = pd.read_csv("/Users/louisadam/Documents/GitHub/Repo_G1_Dataset_2_flower/Dataset_2_flower.csv", sep="|")
+df = pd.read_csv("/Dataset_2_flower.csv", sep="|")
 
 listColumnsOk = ["Id", "SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm", "Species"] # Colonne
 listColumnsNonOk = ["index", "Unnamed: 0", "Unnamed: 0.1", "Unnamed: 0.1.1", "level_0", "Unnamed: 0.1.1.1"]
